6eedf3acab242601a84346e466414546ee87180d
dadaruto <cheladorcas.ruto@gmail.com>
2024-07-17 01:22:00 +0300

    Updated seeds.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  post '/contact', to: 'contacts#create', as: 'contact'
+  get 'blog', to: 'blog_posts#index'
+  get 'portfolio', to: 'portfolio#index'
+  get 'about_me', to: 'about_me#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
 
